d88b0dc0f0262a6e2fbb235bfec0054346ab8f9b
Beatriiz-Cavalcante <biamenezes193@gmail.com>
2025-01-17 09:26:43 -0300

    Utilizando a varíavel chat com a definição do layout e ajuste nos comentários

--- hashtagtreinamentos/SitesESistemas/hashzap.py
+++ hashtagtreinamentos/SitesESistemas/hashzap.py
@@ -1,23 +1,28 @@
-# Hashzap
-# botao de iniciar chat
-# popup para entrar no chat
-# quando entrar no chat: (aparece para todo mundo)
-    # a mensagem que você entrou no chat
-    # o campo e o botão de enviar mensagem
-# a cada mensagem que você envia (aparece para todo mundo)
-    # Nome: Texto da Mensagem
+#Descrição do projeto
 
-#importar o flet (no terminal -> pip indysll flet)
+# Titulo Hashzap
+# botao de "iniciar chat"
+    # popup para entrar no chat
+        #Titulo "Bem vindo ao Haszap"
+        #Campo de input com  placeholder "Digite o seu nome"
+        #Botão "Entrar no chat"
+#popup fecha
+# quando entrar no chat: (aparece para todo mundo, inclusive para você) mensagem que fulano entrou no chat
+#Campo de input com o placeholder "Digite aqui sua mensagem"
+#Botão "enviar mensagem"
+# a cada mensagem que você envia aparece para todo mundo nesse formato -> Nome: Texto da Mensagem
+
+#importar o flet (no terminal -> pip install flet)
 import flet as ft
 
 #Criar uma função principal para rodar o aplicativo
 def main(pagina):
     #titulo (elemento 1)
     titulo = ft.Text("Hashzap")
-
+    #Estilo do chat (vertical)
     chat = ft.Column()
         
-    #função botaopopup
+    #função botaopopup (a função usada pelo botao deve ser definida antes de ser chamada)
     def entrar_chat(evento):
         #fechar popup
         popup.open = False
@@ -25,7 +30,10 @@ def main(pagina):
         pagina.remove(titulo)
         #tirar o botao inicial
         pagina.remove(botao)
+        #Adicionar a definição do layout do chat
+        pagina.add(chat)
         #carregar o chat (elemento 4)
+        #input mensagem (elemento 4.1) 
         campo_enviar_mensagem = ft.TextField(label="Digite aqui sua mensagem")
         #carregar o campo enviar mensagem (elemento 5)
         def enviar_mensagem(evento):
@@ -37,6 +45,7 @@ def main(pagina):
             campo_enviar_mensagem.value = ""
             pagina.update()
         #a função usada pelo botao deve ser definida antes de ser chamada
+        #botão enviar mensagem (elemento 4.2)
         botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
         #aplicando o estilo (botar input e botão lado a lado)
         linha_enviar = ft.Row([campo_enviar_mensagem, botao_enviar])
@@ -47,7 +56,6 @@ def main(pagina):
         texto_mensagem = ft.Text(f"{nome_usuario} entrou no chat")
         chat.controls.append(texto_mensagem)
         pagina.update()
-
 
     #popup (elemento 3)
     titulo_popup = ft.Text("Bem vindo ao Hashzap")
@@ -64,12 +72,10 @@ def main(pagina):
     #a função usada pelo botao deve ser definida antes de ser chamada
     botao = ft.ElevatedButton("Iniciar Chat", on_click=abrir_popup)
     
-
     #colocar elementos na pagina
     pagina.add(titulo)
     pagina.add(botao)
     
-
 #Executar essa função com o flet
-#ft.app(main, view=ft.WEB_BROWSER) #para exibir no navegador é preciso enviar o parâmentro web browser na view, o padrão é o sistema desktop
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER) #para exibir no navegador é preciso enviar o parâmentro web browser na view, o padrão é o sistema desktop
+#ft.app(main)
